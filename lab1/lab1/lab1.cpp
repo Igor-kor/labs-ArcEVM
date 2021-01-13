ede681105ee05cb06e1c7f14f1479998983e6ec4
Igor Sharangiya <igor_korenovsk@mail.ru>
2021-01-13 17:52:06 +0300

    add 1 labs

---
+++ lab1/lab1/lab1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main()
+{
+    DWORD startTime, endTime;
+    startTime = GetTickCount();
+    //выполенние операций
+  
+    endTime = GetTickCount();
+    printf("Work time: %d\n", startTime);
+    return 0;
+}
