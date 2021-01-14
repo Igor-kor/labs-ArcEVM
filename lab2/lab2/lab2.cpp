896651098a7c278ac1debbecd34e211af0e06fa7
Igor Sharangiya <igor_korenovsk@mail.ru>
2021-01-14 14:36:15 +0300

    2 lab done

--- lab2/lab2/lab2.cpp
+++ lab2/lab2/lab2.cpp
@@ -1,5 +1,4 @@
-﻿// lab2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿// все функции https://doc.rust-lang.org/core/arch/x86_64/fn._mm_sub_ps.html
 
 #include <stdio.h>
 #include <cmath>
@@ -11,29 +10,31 @@
 
 // вариант 1 (TODO переделать на функцию)
 
-//Вычисляем функцию через cpp: y = sqrt(x + 0.5)
+//Вычисляем функцию через cpp: x^2+y-sqrt(z^2+1)
 void ComputeArrayCpp(
-	float* pArray, // [in] исходный массив
+	float* pArrayx, // [x] исходный массив
+	float* pArrayy, // [y] исходный массив
+	float* pArrayz, // [z] исходный массив
 	float* pResult, // [out] массив результатов
 	int nSize) // [in] размер массивов
 {
-	int i;
-
-	float* pSource = pArray;
+	float* pSourcex = pArrayx;
+	float* pSourcey = pArrayy;
+	float* pSourcez = pArrayz;
 	float* pDest = pResult;
 
-	for (i = 0; i < nSize; i++)
+	for (int i = 0; i + 2 < nSize; i++)
 	{
-		*pDest = (float)sqrt((*pSource) + 0.5f);
-
+		*pDest = (float)*(pSourcex + i) * *(pSourcex + i) + *(pSourcey + i) - sqrt((*(pSourcez + i)) * (*(pSourcez + i)) + 1.0f);
 		//Перемещаемся к следующему элементу
-		pSource++;
 		pDest++;
 	}
 }
-//Вычисляем функцию через sse: y = sqrt(x + 0.5)
+//Вычисляем функцию через sse: /*y = sqrt(x + 0.5)*/  x^2+y-sqrt(z^2+1)
 void ComputeArrayCppSSE(
-	float* pArray, // [in] исходный массив
+	float* pArrayx, // [x] исходный массив
+	float* pArrayy, // [y] исходный массив
+	float* pArrayz, // [z] исходный массив
 	float* pResult, // [out] массив результатов
 	int nSize) // [in] размер массивов
 {
@@ -41,23 +42,28 @@ void ComputeArrayCppSSE(
 	//sizeof(__m128)/sizeof(float) => 4
 	int nLoop = nSize / 4;
 
-	__m128 m1;
+	__m128 m2;
 
 	//Преобразуем указатели на float к типу __m128
-	__m128* pSrc = (__m128*) pArray;
+	__m128* pSrcx = (__m128*) pArrayx;
+	__m128* pSrcy = (__m128*) pArrayy;
+	__m128* pSrcz = (__m128*) pArrayz;
 	__m128* pDest = (__m128*) pResult;
 
 
-	__m128 m0_5 = _mm_set_ps1(0.5f); // m0_5[0, 1, 2, 3] = 0.5
+	__m128 m1 = _mm_set_ps1(1); // m0_5[0, 1, 2, 3] = 1
 
 	for (int i = 0; i < nLoop; i++)
 	{
-		m1 = _mm_add_ps(*pSrc, m0_5); // m1 = *pSrc + 0.5
-		*pDest = _mm_sqrt_ps(m1); // *pDest = sqrt(m1)
+
+		m2 = _mm_add_ps(_mm_mul_ps( *pSrcz, *pSrcz), m1); // m1 = *pSrcz * *pSrcz + 1
+		*pDest = _mm_sub_ps(_mm_add_ps( _mm_mul_ps(*pSrcx, *pSrcx), *pSrcy ), _mm_sqrt_ps(m2)); // *pDest = sqrt(m2)
 
 		//Перемещаемся к следующему элементу __m128
 		//Тем самым пропускаем 4 float из массива
-		pSrc++;
+		pSrcx++;
+		pSrcy++;
+		pSrcz++;
 		pDest++;
 	}
 }
@@ -80,10 +86,14 @@ int main(int argc, char* argv[])
 	//Тем самым пропускаем 4 float из массива
 	float* x = (float*)_aligned_malloc(sizeof(float) * MAX_SIZE, 16);
 	float* y = (float*)_aligned_malloc(sizeof(float) * MAX_SIZE, 16);
+	float* z = (float*)_aligned_malloc(sizeof(float) * MAX_SIZE, 16);
+	float* out = (float*)_aligned_malloc(sizeof(float) * MAX_SIZE, 16);
 	DWORD startTime, endTime;
 	//Получаем время начала работы
 	startTime = GetTickCount();
 	init(x, MAX_SIZE);
+	init(y, MAX_SIZE);
+	init(z, MAX_SIZE);
 	//Получаем время окончания работы
 	endTime = GetTickCount();
 
@@ -92,7 +102,7 @@ int main(int argc, char* argv[])
 	startTime = GetTickCount();
 
 	//Выполняем вычисления с помощью обычных функций c++
-	ComputeArrayCpp(x, y, MAX_SIZE);
+	ComputeArrayCpp(x, y, z, out, MAX_SIZE);
 
 	endTime = GetTickCount();
 
@@ -101,7 +111,7 @@ int main(int argc, char* argv[])
 	startTime = GetTickCount();
 
 	//Выполняем вычисления с помощью SSE
-	ComputeArrayCppSSE(x, y, MAX_SIZE);
+	ComputeArrayCppSSE(x, y, z, out, MAX_SIZE);
 
 	endTime = GetTickCount();
 
@@ -110,6 +120,8 @@ int main(int argc, char* argv[])
 	//Освобождаем память массивов
 	_aligned_free(x);
 	_aligned_free(y);
+	_aligned_free(z);
+	_aligned_free(out);
 
 	return 0;
 }
